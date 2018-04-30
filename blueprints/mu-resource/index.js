6d8738fb2516e0878f73b983d569ec0e0a8b7a93
Erika Pauwels <erika.pauwels@gmail.com>
2018-04-30 16:16:12 +0200

    Update custom inflector rules with validations and bbcdr reports

--- blueprints/mu-resource/index.js
+++ blueprints/mu-resource/index.js
@@ -45,6 +45,13 @@ inflector.irregular("editor-document", "editor-documents");
 inflector.irregular("editor-document-status", "editor-document-statuses");
 inflector.irregular("export", "exports");
 inflector.irregular("account", "accounts");
+inflector.irregular('identificator', 'identificatoren');
+inflector.irregular('file', 'files');
+inflector.irregular('document-status', 'document-statuses');
+inflector.irregular('bbcdr-report', 'bbcdr-reports');
+inflector.irregular('validation', 'validations');
+inflector.irregular('validation-execution', 'validation-executions');
+inflector.irregular('validation-error', 'validation-errors');
 
 /* eslint-env node */
 module.exports = {
